84b9af3d7c307e9a900c61b42864010f1393d217
Harrix <sergienkoanton@mail.ru>
2014-02-12 02:10:45 +0800

    Обновления библиотек

--- source_make/HarrixQtLibrary.cpp
+++ source_make/HarrixQtLibrary.cpp
@@ -1,5 +1,5 @@
 //HarrixQtLibrary
-//Версия 3.17
+//Версия 3.18
 //Сборник функций для Qt.
 //https://github.com/Harrix/HarrixQtLibrary
 //Библиотека распространяется по лицензии Apache License, Version 2.0.
@@ -2034,3 +2034,102 @@ double HQt_QStringToNumber (QString x, bool checkcomma)
     return VMHL_Result;
 }
 //---------------------------------------------------------------------------
+
+bool HQt_NaturalCompareTwoQStrings(const QString& s1,const QString& s2)
+{
+    /*
+    Функция сравнивает две строки и определяет какая строчка идет первой. Служебная функция для сортировки строк
+    в обычном стиле, когда строки: z1, z10, z2 сортируются как z1, z2, z10.
+    Входные параметры:
+     s1 - первая строка;
+     s2 - вторая строка.
+    Возвращаемое значение:
+     false - когда вторая строка должна быть первой;
+     true - когда первая строка должна быть первой.
+    Примечание:
+     Функция взята отсюда: http://www.qtcentre.org/archive/index.php/t-21411.html
+    */
+    // ignore common prefix..
+    int i = 0;
+    while ((i < s1.length()) && (i < s2.length()) && (s1.at(i).toLower() == s2.at(i).toLower()))
+        ++i;
+    ++i;
+    // something left to compare?
+    if ((i < s1.length()) && (i < s2.length()))
+    {
+        // get number prefix from position i - doesnt matter from which string
+        int k = i-1;
+        //If not number return native comparator
+        if(!s1.at(k).isNumber() || !s2.at(k).isNumber())
+        {
+            //Two next lines
+            //E.g. 1_... < 12_...
+            if(s1.at(k).isNumber())
+                return false;
+            if(s2.at(k).isNumber())
+                return true;
+            return QString::compare(s1, s2, Qt::CaseSensitive) < 0;
+        }
+        QString n = "";
+        k--;
+        while ((k >= 0) && (s1.at(k).isNumber()))
+        {
+            n = s1.at(k)+n;
+            --k;
+        }
+        // get relevant/signficant number string for s1
+        k = i-1;
+        QString n1 = "";
+        while ((k < s1.length()) && (s1.at(k).isNumber()))
+        {
+            n1 += s1.at(k);
+            ++k;
+        }
+
+        // get relevant/signficant number string for s2
+        //Decrease by
+        k = i-1;
+        QString n2 = "";
+        while ((k < s2.length()) && (s2.at(k).isNumber()))
+        {
+            n2 += s2.at(k);
+            ++k;
+        }
+
+        // got two numbers to compare?
+        if (!n1.isEmpty() && !n2.isEmpty())
+            return (n+n1).toInt() < (n+n2).toInt();
+        else
+        {
+            // not a number has to win over a number.. number could have ended earlier... same prefix..
+            if (!n1.isEmpty())
+                return false;
+            if (!n2.isEmpty())
+                return true;
+            return s1.at(i) < s2.at(i);
+        }
+    }
+    else
+    {
+        // shortest string wins
+        return s1.length() < s2.length();
+    }
+}
+//---------------------------------------------------------------------------
+
+QStringList HQt_NaturalSortingQStringList (QStringList StringList)
+{
+    /*
+    Функция сортировки строк в сортировки строк QStringList в натуральном виде, например,
+    строки: z1, z10, z2 сортируются как z1, z2, z10.
+    Входные параметры:
+     StringList - сортируемый список строк.
+    Возвращаемое значение:
+     Отсортированный список строк.
+    */
+    QStringList SortList=StringList;
+
+    qSort(SortList.begin(),SortList.end(),HQt_NaturalCompareTwoQStrings);
+
+    return SortList;
+}
